f8519902afae626684f8be6654b0e56434e72e75
Max <maxprovin01@gmail.com>
2020-03-15 18:19:00 +0000

    echo: 2 variations added

--- chapter1/echo/echo1.go
+++ chapter1/echo/echo1.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+		s += sep + os.Args[i] // os.Args first element is the name of the command itself
 		sep = " "
 	}
 	fmt.Println(s)
