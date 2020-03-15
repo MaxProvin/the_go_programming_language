4895652bd9fa4d7ac79d8da77e49c097ee534a77
Max <maxprovin01@gmail.com>
2020-03-15 18:30:43 +0000

    chapter 1, exercise 1.1 & 1.2

---
+++ chapter1/exercises/1.2.go
@@ -0,0 +1,16 @@
+/*
+	1.2 Modify the echo program to print the index and value of each of its arguments
+	one per line
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for idx, value := range os.Args[1:] {
+		fmt.Println(idx, value)
+	}
+}
